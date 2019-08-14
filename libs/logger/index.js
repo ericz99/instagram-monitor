454d2963e419561ab15e2be36a217b62d8d003f5
Eric <ezhang91@yahoo.com>
2019-08-14 17:13:37 -0400

    updated logging

--- libs/logger/index.js
+++ libs/logger/index.js
@@ -5,26 +5,35 @@
 import dateFormat from 'dateformat';
 import colors from 'colors';
 
+const Logger = function Logger(name) {
+  this._name = name;
+};
+
+Logger.prototype.green = function success(message) {
+  console.log(`${getDateString()} [${this._name}] ` + colors.green('[+] ' + message));
+};
+
+Logger.prototype.red = function error(message) {
+  console.log(`${getDateString()} [${this._name}] ` + colors.red('[x] ' + message));
+};
+
+Logger.prototype.blue = function won(message) {
+  console.log(`${getDateString()} [${this._name}] ` + colors.blue('[$] ' + message));
+};
+
+Logger.prototype.normal = function info(message) {
+  console.log(`${getDateString()} [${this._name}] [#] ${message}`);
+};
+
+Logger.prototype.yellow = function caution(message) {
+  console.log(`${getDateString()} [${this._name}] ` + colors.yellow('[?] ' + message));
+};
+
 function getDateString() {
   return '[' + dateFormat(new Date(), 'HH:MM:ss.l') + ']';
 }
 
 // export for use elsewhere
-export default (type, message) => {
-  switch (type) {
-    case 'success':
-      console.log(`${getDateString()} [${type}] ` + colors.green('[+] ' + message));
-      break;
-    case 'info':
-      console.log(`${getDateString()} [${type}] ` + colors.blue('[$] ' + message));
-      break;
-    case 'warn':
-      console.log(`${getDateString()} [${type}] ` + colors.yellow('[?] ' + message));
-      break;
-    case 'error':
-      console.log(`${getDateString()} [${type}] ` + colors.red('[x] ' + message));
-      break;
-    default:
-      break;
-  }
+export default name => {
+  return new Logger(name);
 };
