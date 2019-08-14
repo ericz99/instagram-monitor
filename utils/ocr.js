d9eeeaab571e5ff48dc29a8f866bd90b9a586a8a
Eric <ezhang91@yahoo.com>
2019-08-13 23:39:45 -0400

    Added OCR Support

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,1 +1,15 @@
-import recognize from 'tesseractocr';
+import Tesseract from 'tesseract.js';
+import { sendOcr } from '../libs/notify';
+import config from '../config/config.json';
+
+const { TesseractWorker } = Tesseract;
+const worker = new TesseractWorker();
+
+module.exports = async image => {
+  const { text } = await worker.recognize(image);
+  if (text) {
+    await sendOcr(config, text);
+    await worker.terminate();
+    return;
+  }
+};
