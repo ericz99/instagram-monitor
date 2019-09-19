d11bfc377132a7cfbab34708785c162f2c895edb
Eric <ezhang91@yahoo.com>
2019-09-18 22:05:37 -0400

    updated notification webhook

--- libs/notify/index.js
+++ libs/notify/index.js
@@ -24,11 +24,53 @@ export const sendNotify = async (
           fields: [
             {
               name: 'Caption',
-              value: metadata.post.text
+              value: metadata.post.text == null ? '[Post has no caption]' : metadata.post.text
             }
           ],
           image: {
             url: metadata.post.displayUrl
+          },
+          footer: {
+            text: footertext !== '' ? footertext : 'Custom Instagram Monitor v1',
+            icon_url: footericon !== '' ? footericon : null
+          }
+        }
+      ]
+    }
+  };
+
+  await request(options);
+};
+
+export const sendStoryNotify = async (
+  { discord: { webhook_url, username, avatar, footertext, footericon } },
+  metadata
+) => {
+  const options = {
+    uri: webhook_url,
+    method: 'POST',
+    json: true,
+    body: {
+      username,
+      avatar_url: avatar !== '' ? avatar : null,
+      embeds: [
+        {
+          author: {
+            name: metadata.userName,
+            url: `https://www.instagram.com/${metadata.userName}`,
+            icon_url: metadata.imageUrl
+          },
+          title: `${metadata.userName} - Story Link`,
+          url: `https://www.instagram.com/stories/${metadata.userName}/${metadata.story.storyID}`,
+          color: 15258703,
+          fields: [
+            {
+              name: 'isVideo',
+              value: metadata.story.isVideo
+            }
+          ],
+          image: {
+            url: metadata.story.displayUrl
           },
           footer: {
             text: footertext !== '' ? footertext : 'Custom Instagram Monitor v1',
